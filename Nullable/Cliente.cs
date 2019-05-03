ceed57af47c8b0386342094a52b4b89561c73966
Alberto Osinaga <luis.osinaga@gmail.com>
2019-05-03 11:13:05 -0400

    Cliente Constructo con valores max disp crudo

--- Nullable/Cliente.cs
+++ Nullable/Cliente.cs
@@ -10,6 +10,15 @@ namespace Nullable
         public DateTime PrimeraCompra;
         public DateTime UltimaCompra;
 
+        public Cliente()
+        {
+            Nombre = null;
+            NroCompras = int.MaxValue;
+            MontoCompras = decimal.MaxValue;
+            PrimeraCompra = DateTime.MinValue;
+            UltimaCompra = DateTime.MinValue;
+        }
+
 
         public override string ToString() => $"\nCliente: {Nombre}" +
                                             $"\nNro de Compras: {NroCompras}" +
